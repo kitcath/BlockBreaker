305b0c3e09a9bb2a31148c954287ef45f3891c87
kitcath <catherine.vangheluwe@mail.mcgill.ca>
2020-05-20 23:37:36 -0400

    First GUI

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void nextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void startScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+   
+}
